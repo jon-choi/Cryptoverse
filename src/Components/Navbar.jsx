a1a93e10df8a0a9c1b69265b0a1038452a806318
Jonathan Choi <jonchoi11@gmail.com>
2021-11-02 17:26:39 -0600

    add links for navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,6 +17,20 @@ const Navbar = () => {
           </Button> */}
         </Typography.Title>
       </div>
+      <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined/>}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+      </Menu>
       
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-reactDom.render(
-  <Router>
-    <App />
-  </Router>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );
