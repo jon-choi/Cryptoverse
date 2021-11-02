eff2b340d09e5114e5bd6ed254a0fb901f276297
Jonathan Choi <jonchoi11@gmail.com>
2021-11-02 17:08:58 -0600

    install router-dom, create index for components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,13 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
+import icon from '../images/cryptocurrency.png';
+
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar />
+        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
           {/* <Button className="menu-control-container">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import reactDom from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-reactDom.render(<App />, document.getElementById("root"));
+reactDom.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
