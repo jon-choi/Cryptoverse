d535ec4989f3c60ad942ea7003388b2e5d971937
Jonathan Choi <jonchoi11@gmail.com>
2021-11-02 17:33:08 -0600

    create routes for components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./Components";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./Components";
 import "./App.css";
 
 const App = () => {
@@ -11,7 +18,29 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+                <Route exact path="/news">
+                  <News />
+                </Route>
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
