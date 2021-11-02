94198914c13902d185b76be7a6cb2c91ec0e37ff
Jonathan Choi <jonchoi11@gmail.com>
2021-11-02 16:58:21 -0600

    add nav component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+          {/* <Button className="menu-control-container">
+
+          </Button> */}
+        </Typography.Title>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar;
