64e7a9945430a30f98f9af29b00f89369dbd6330
Jonathan Choi <jonchoi11@gmail.com>
2021-11-02 19:50:15 -0600

    show search only on currency page

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -24,9 +24,12 @@ const Cryptocurrencies = ({simplified}) => {
 
   return (
     <>
-    <div className="search-crypto">
+    {!simplified && (
+      <div className="search-crypto">
       <Input placeholder="Search Cryptocurrencies" onChange={(e) => setSearchTerm(e.target.value)} />
     </div>
+    )}
+    
     <Row gutter={[32, 32]} className="crypto-card-container">
       {cryptos?.map((currency) => (
         <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
