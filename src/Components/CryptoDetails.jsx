d0e4c5dec819e3018ceb9ee0a0f2c3bcba04054a
Jonathan Choi <jonchoi11@gmail.com>
2021-11-03 15:54:45 -0600

    add mobile responsiveness

--- src/Components/CryptoDetails.jsx
+++ src/Components/CryptoDetails.jsx
@@ -33,7 +33,7 @@ const CryptoDetails = () => {
   const genericStats = [
     { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
     { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Approved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
     { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
     { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
   ];
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
@@ -6,18 +6,42 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
+
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('reaize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize])
+
   return (
     <div className="nav-container">
       <div className="logo-container">
         <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
-          {/* <Button className="menu-control-container">
-
-          </Button> */}
+          <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+            <MenuOutlined />
+          </Button>
         </Typography.Title>
       </div>
-      <Menu theme="dark">
+      {activeMenu && (
+        <Menu theme="dark">
           <Menu.Item icon={<HomeOutlined />}>
             <Link to="/">Home</Link>
           </Menu.Item>
@@ -31,7 +55,7 @@ const Navbar = () => {
             <Link to="/news">News</Link>
           </Menu.Item>
       </Menu>
-      
+      )}
     </div>
   )
 }
