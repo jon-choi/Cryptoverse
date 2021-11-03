d745da137487dff7e6e006f940c639acb197e781
Jonathan Choi <jonchoi11@gmail.com>
2021-11-03 16:11:58 -0600

    add boldness to font

--- src/Components/Exchanges.jsx
+++ src/Components/Exchanges.jsx
@@ -18,10 +18,10 @@ const Exchanges = () => {
   return (
     <>
       <Row>
-        <Col span={6}>Exchanges</Col>
-        <Col span={6}>24h Trade Volume</Col>
-        <Col span={6}>Markets</Col>
-        <Col span={6}>Change</Col>
+        <Col span={6}><b>Exchanges</b></Col>
+        <Col span={6}><b>24h Trade Volume</b></Col>
+        <Col span={6}><b>Markets</b></Col>
+        <Col span={6}><b>Change</b></Col>
       </Row>
       <Row>
         {exchangesList.map((exchange) => (
