68f3a006f720516109302433db8e2696a9887960
Jonathan Choi <jonchoi11@gmail.com>
2021-11-03 13:39:25 -0600

    create chart for stats

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -5,6 +5,42 @@ import { Col, Row, Typography } from 'antd';
 const { Title } = Typography;
 
 const LineChart = ({coinHistory, currentPrice, coinName}) => {
+
+  const coinPrice = [];
+  const coinTimeStamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory.data.history[i].price)
+    coinTimeStamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
+  };
+
+  const data = {
+    labels: coinTimeStamp,
+    datasets: [
+      {
+        label: 'Price in USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      }
+    ]
+  }
+
+  const options = {
+    scales: {
+      yAxis: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+
+
+  
   return (
     <>
     <Row className="chart-header">
@@ -14,6 +50,7 @@ const LineChart = ({coinHistory, currentPrice, coinName}) => {
         <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
       </Col>
     </Row>
+    <Line data={data} options={options} />
     </>
   )
 }
