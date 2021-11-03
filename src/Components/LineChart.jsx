d9c712db1c56ce84f5fd57a5b720522954171614
Jonathan Choi <jonchoi11@gmail.com>
2021-11-03 13:35:04 -0600

    add currency details for component

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -4,12 +4,18 @@ import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
-const LineChart = () => {
+const LineChart = ({coinHistory, currentPrice, coinName}) => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Row className="chart-header">
+      <Title level={2} clasName="chart-title">{coinName} Price Chart</Title>
+      <Col className="price-container">
+        <Title level={5} className="price-change">{coinHistory?.data?.change}%</Title>
+        <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+      </Col>
+    </Row>
+    </>
   )
 }
 
-export default LineChart
+export default LineChart;
