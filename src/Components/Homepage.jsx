d156137e762e39f034b179acd1a69b28a0622757
Jonathan Choi <jonchoi11@gmail.com>
2021-11-03 16:03:49 -0600

    import loader into all components

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Cryptocurrencies, News } from '../Components';
+import Loader from './Loader';
 
 const { Title } = Typography;
 
@@ -14,7 +15,7 @@ const Homepage = () => {
 
   console.log(data);
 
-  if (isFetching) return 'Loading... ';
+  if (isFetching) return <Loader />;
 
 
   return (
