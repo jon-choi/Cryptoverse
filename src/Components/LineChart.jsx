6ab1730512f9f45c06356ae984e291815516d1b3
Jonathan Choi <jonchoi11@gmail.com>
2022-02-14 14:56:52 -0700

    fix typo

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -14,7 +14,7 @@ const LineChart = ({coinHistory, currentPrice, coinName}) => {
   }
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+    coinTimeStamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
   }
 
   const data = {
